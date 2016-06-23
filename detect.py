964fcc1ab2057b17adb550938757c8a533e8f94b
Syed Ahmed <syed.hope@gmail.com>
2016-06-23 19:08:46 -0400

    Added a method to detect humans
    Re-organized the code for human detection

--- detect.py
+++ detect.py
@@ -15,6 +15,45 @@ def find_nearest(array,value): # function to find the nearest value in an array 
     x = (numpy.abs(array-value)).argmin()
     return array[x]
 
+def detect_human(Images): # the function accpets an image and draws a bbox in the resulting image, around the human if any.
+    for file in Images:
+	I = cv2.imread(path+'/'+file) # Input image
+	Ig = cv2.cvtColor( I, cv2.COLOR_RGB2GRAY ) # grayscale of input image
+	r,c = Ig.shape # Image resolution
+	Ix = cv2.GaussianBlur(Ig, (5, 5), 0) # Blurring with Gaussian kernel of 5 and standard deviation of 0 in X direction
+	#Applying Otsu binarization to segment the image...
+	th_val, Iseg = cv2.threshold(Ix, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+	bgrnd = numpy.count_nonzero(Iseg) # nadir pixel count
+	fgrnd = numpy.size(Ig)-bgrnd # highlighted pixel count
+	if bgrnd > fgrnd: #Making sure that Human gets segmented and not the background 
+	   Iseg = ~Iseg 
+	Creg = morphology.label(Iseg) # connected region of the segmented image
+	Preg = measure.regionprops(Creg) # Properties of the connected regions
+	temp = [[0, 0, 0, 0, 0]] # storing the parameters of the required bbox
+	for i in Preg:
+            if i.area >100:
+                ltcy, ltcx, rbcy, rbcx = i.bbox
+                # retrieve x,y locations of the left top corner and right bottom corner pertaining to the bbox
+                temp.append([ltcx,ltcy,rbcx,rbcy,(rbcx - ltcx)*(rbcy - ltcy)])
+	temp = numpy.delete(temp, (0), axis=0) # deleting the row with zeros
+	ind = numpy.argmax(temp[:,4]) # Index of the segment with biggest area
+	ltcx,rbcx = numpy.min(temp[:,0]),max(temp[:,2]) # x pixel location of corners of bbox# temp[ind,0] #temp[ind,2]
+	ltcy,rbcy = numpy.min(temp[:,1]),numpy.max(temp[:,3]) # y pixel location of corners of bbox
+	H,W = rbcy - ltcy,rbcx - ltcx # Height and Width of the bbox
+	if len(temp)>35 and 150<(H-W)<1000: # If too many segments found 
+            ltcy,rbcy = temp[ind,1],temp[ind,3] # y pixel location corresponding to the biggest segment
+            new = [[0, 0, 0, 0, 0]]
+            XrangeA = (I.shape[1]/2)-100 # defining the range in which the bbox is required
+            XrangeB = (I.shape[1]/2)+100
+            for i in range(ind-4,ind+5): # selecting the segments around the biggest one
+                new.append(temp[i])
+            new = numpy.array(new)
+            ltcx,rbcx=find_nearest(new[:,0],XrangeA),find_nearest(new[:,2],XrangeB) # selecting the x locations within the chosen range
+	H,W = rbcy - ltcy,rbcx - ltcx# Height and Width of the bbox
+	# Finally Drawing the bounding box
+	cv2.rectangle(I, (ltcx, ltcy), (ltcx + W, ltcy + H), (198, 245, 174), 4)
+	cv2.imwrite(dir+file,I)
+		
 dir = './results/'  # required output folder
 if not os.path.exists(dir):
     os.makedirs(dir)
@@ -28,41 +67,4 @@ if __name__ == '__main__':
     if len(sys.argv) > 2:
         ext = sys.argv[2:] # optional extensions
     Images = [i for i in os.listdir(path) if any( [i.endswith(j) for j in ext])] # array with names of input images 
-
-for file in Images:
-    I = cv2.imread(path+'/'+file) # Input image
-    Ig = cv2.cvtColor( I, cv2.COLOR_RGB2GRAY ) # grayscale of input image
-    r,c = Ig.shape # Image resolution
-    Ix = cv2.GaussianBlur(Ig, (5, 5), 0) # Blurring with Gaussian kernel of 5 and standard deviation of 0 in X direction
-    #Applying Otsu binarization to segment the image...
-    th_val, Iseg = cv2.threshold(Ix, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    bgrnd = numpy.count_nonzero(Iseg) # nadir pixel count
-    fgrnd = numpy.size(Ig)-bgrnd # highlighted pixel count
-    if bgrnd > fgrnd: #Making sure that Human gets segmented and not the background 
-    	Iseg = ~Iseg 
-    Creg = morphology.label(Iseg) # connected region of the segmented image
-    Preg = measure.regionprops(Creg) # Properties of the connected regions
-    temp = [[0, 0, 0, 0, 0]] # storing the parameters of the required bbox
-    for i in Preg:
-    	if i.area >100:
-    		ltcy, ltcx, rbcy, rbcx = i.bbox
-    		# retrieve x,y locations of the left top corner and right bottom corner pertaining to the bbox
-    		temp.append([ltcx,ltcy,rbcx,rbcy,(rbcx - ltcx)*(rbcy - ltcy)])
-    temp = numpy.delete(temp, (0), axis=0) # deleting the row with zeros
-    ind = numpy.argmax(temp[:,4]) # Index of the segment with biggest area
-    ltcx,rbcx = numpy.min(temp[:,0]),max(temp[:,2]) # x pixel location of corners of bbox# temp[ind,0] #temp[ind,2]
-    ltcy,rbcy = numpy.min(temp[:,1]),numpy.max(temp[:,3]) # y pixel location of corners of bbox
-    H,W = rbcy - ltcy,rbcx - ltcx # Height and Width of the bbox
-    if len(temp)>35 and 150<(H-W)<1000: # If too many segments found 
-        ltcy,rbcy = temp[ind,1],temp[ind,3] # y pixel location corresponding to the biggest segment
-        new = [[0, 0, 0, 0, 0]]
-        XrangeA = (I.shape[1]/2)-100 # defining the range in which the bbox is required
-        XrangeB = (I.shape[1]/2)+100
-        for i in range(ind-4,ind+5): # selecting the segments around the biggest one
-            new.append(temp[i])
-        new = numpy.array(new)
-        ltcx,rbcx=find_nearest(new[:,0],XrangeA),find_nearest(new[:,2],XrangeB) # selecting the x locations within the chosen range
-    H,W = rbcy - ltcy,rbcx - ltcx# Height and Width of the bbox
-    # Finally Drawing the bounding box
-    cv2.rectangle(I, (ltcx, ltcy), (ltcx + W, ltcy + H), (198, 245, 174), 4)
-    cv2.imwrite(dir+file,I)
+    detect_human(Images)
